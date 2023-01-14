90c3a2d0f7795f888e2551ce1cce4c7f78ebe069
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-01-14 12:49:37 +0700

    sourceCode

--- A1022I1_module_2_tran_trieu/src/Ss03_array_and_method/Method/pratice/Demo.java
+++ A1022I1_module_2_tran_trieu/src/Ss03_array_and_method/Method/pratice/Demo.java
@@ -1,2 +1,18 @@
-package Ss03_array_and_method.Method.pratice;public class Demo {
+package Ss03_array_and_method.Method.pratice;
+
+public class Demo {
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static int sum1(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+//        sum(5, 6);
+        int result = sum1(1, 2);
+//        int result =sum(1,2);//method is not return value
+        System.out.println(result);
+    }
 }
--- A1022I1_module_2_tran_trieu/src/Ss03_array_and_method/Method/pratice/Test.java
+++ A1022I1_module_2_tran_trieu/src/Ss03_array_and_method/Method/pratice/Test.java
@@ -1,2 +1,9 @@
-package Ss03_array_and_method.Method.pratice;public class Test {
+package Ss03_array_and_method.Method.pratice;
+
+public class Test {
+    public static void main(String[] args) {
+        Demo.sum(5,6);//Static
+/*        Demo object = new Demo();
+        object.sum(8,9);*/ //Non Static
+    }
 }
