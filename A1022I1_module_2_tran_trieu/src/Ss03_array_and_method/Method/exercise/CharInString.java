e7663b7544765af19253a56b5253c45391ce7af7
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-01-15 10:08:13 +0700

    bt_ss03_method

---
+++ A1022I1_module_2_tran_trieu/src/Ss03_array_and_method/Method/exercise/CharInString.java
@@ -0,0 +1,20 @@
+package Ss03_array_and_method.Method.exercise;
+
+import java.util.Scanner;
+
+public class CharInString {
+    static String myString = "Hello World";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1 character:");
+        char c = sc.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < myString.length(); i++) {
+            if (c == myString.charAt(i)) {
+                count++;
+            }
+        }
+        System.out.println(c + " appears " + count + " times in String");
+    }
+}
