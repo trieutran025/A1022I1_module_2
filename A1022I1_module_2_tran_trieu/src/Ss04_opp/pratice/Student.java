97d26c351fd54dac6411f23207faa93201220099
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-02-02 18:19:05 +0700

    pr_ss04

--- A1022I1_module_2_tran_trieu/src/Ss04_opp/pratice/Student.java
+++ A1022I1_module_2_tran_trieu/src/Ss04_opp/pratice/Student.java
@@ -1,2 +1,46 @@
-package Ss04_opp.pratice;public class Student {
+package Ss04_opp.pratice;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student() {
+        this("no name");
+        System.out.println(1);
+    }
+
+    public Student(String name) {
+        System.out.println(2);
+        this.name = name;
+    }
+
+    public Student(int id) {
+
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        Student student2 = new Student();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- A1022I1_module_2_tran_trieu/src/Ss04_opp/pratice/Test.java
+++ A1022I1_module_2_tran_trieu/src/Ss04_opp/pratice/Test.java
@@ -1,2 +1,17 @@
-package Ss04_opp.pratice;public class Test {
+package Ss04_opp.pratice;
+
+import java.sql.SQLOutput;
+
+public class Test {
+    public static void main(String[] args) {
+//        Student student1 = new Student(1,"Hung");
+        Student student2= new Student();
+        System.out.println(student2.getName());
+        student2.setName("Minh");
+        System.out.println(student2.getName());
+        Student student3 = new Student(10);
+        System.out.println(student3.getId());
+        Student student4= new Student("An");
+        System.out.println(student4.getName());
+    }
 }
