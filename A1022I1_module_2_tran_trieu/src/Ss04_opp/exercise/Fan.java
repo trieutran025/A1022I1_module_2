ab1cd67b85f859a98d6409abe36a42e9331da32e
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-02-02 18:11:02 +0700

    bt_ss04_opp

---
+++ A1022I1_module_2_tran_trieu/src/Ss04_opp/exercise/Fan.java
@@ -0,0 +1,81 @@
+package Ss04_opp.exercise;
+
+public class Fan {
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Fan() {
+        this.speed = SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        if (isOn() == true) {
+            return "Fan{" +
+                    "speed=" + speed +
+                    ", radius=" + radius +
+                    ", color='" + color + "' fan is on" +
+                    '}';
+        } else {
+            return "Fan{" +
+                    "speed=" + speed +
+                    ", radius=" + radius +
+                    ", color='" + color + "' fan is off" +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        Fan fanFirst = new Fan(FAST, true, 10, "yellow");
+        Fan fanSecond = new Fan(MEDIUM, false, 5, "blue");
+        System.out.println(fanFirst);
+        System.out.println(fanSecond);
+    }
+}
