c2a43f0a7585e824b6ead5fb22b60d473c3b0b44
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-02-11 09:52:43 +0700

    src

--- A1022I1_module_2_tran_trieu/src/ss07_abstract_interface/exercise/colorable/Square.java
+++ A1022I1_module_2_tran_trieu/src/ss07_abstract_interface/exercise/colorable/Square.java
@@ -2,11 +2,21 @@ package ss07_abstract_interface.exercise.colorable;
 
 public class Square implements Colorable {
     private double squareEdge;
-
+    private String color;
     @Override
     public void howToColor() {
         System.out.println("Color all four sides");
     }
+
+    public Square(double squareEdge, String color) {
+        this.squareEdge = squareEdge;
+        this.color = color;
+    }
+
+    public Square(String color) {
+        this.color = color;
+    }
+
 
     public Square() {
     }
@@ -27,11 +37,19 @@ public class Square implements Colorable {
         return getSquareEdge() * getSquareEdge();
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     @Override
     public String toString() {
         return "Square{" +
                 "squareEdge=" + squareEdge +";"+
-                "Area of Square"+getArea()+
+                "Area of Square="+getArea()+
                 '}';
     }
 }
--- A1022I1_module_2_tran_trieu/src/ss07_abstract_interface/exercise/colorable/TestShapeColorable.java
+++ A1022I1_module_2_tran_trieu/src/ss07_abstract_interface/exercise/colorable/TestShapeColorable.java
@@ -3,12 +3,14 @@ package ss07_abstract_interface.exercise.colorable;
 public class TestShapeColorable {
     public static void main(String[] args) {
        Square [] squares = new Square[3];
-       squares[0]=new Square(5.6);
-       squares[1]=new Square();
-       squares[2]=new Square(2.0);
+       squares[0]=new Square(5.6,"Blue");
+       squares[1]=new Square(10.0);
+       squares[2]=new Square(2.0,"Green");
         for (Square square:squares) {
-            System.out.println(square.getArea());
-            square.howToColor();
+            System.out.println(square);
+            if(square.getColor()!=null){
+                square.howToColor();
+            }
         }
 
     }
