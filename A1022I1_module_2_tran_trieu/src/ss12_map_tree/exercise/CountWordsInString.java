c2e3e75d238a36215abd438903a4ecbdad3057e3
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-02-24 20:22:34 +0700

    src

---
+++ A1022I1_module_2_tran_trieu/src/ss12_map_tree/exercise/CountWordsInString.java
@@ -0,0 +1,31 @@
+package ss12_map_tree.exercise;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class CountWordsInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String:");
+        String str = sc.nextLine();
+         str = str.toLowerCase();
+        Map<String, Integer> myMap = new TreeMap<>();
+        String[] arr = str.split(" ");
+        int count;
+        String word;
+        for (int i = 0; i < arr.length; i++) {
+            count=1;
+            word = arr[i];
+            if (myMap.containsKey(word)) {
+                myMap.put(word, count + 1);
+            }
+            else {
+                myMap.put(word,count);
+            }
+        }
+        for (String key : myMap.keySet()) {
+            System.out.println(key + ":" + myMap.get(key) + " times");
+        }
+    }
+}
