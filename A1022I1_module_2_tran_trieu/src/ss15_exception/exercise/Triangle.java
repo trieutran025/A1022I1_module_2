5ce2653030c4ff2d05f5e5495cab4818f9004ce4
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-03-06 18:05:21 +0700

    src

--- A1022I1_module_2_tran_trieu/src/ss15_exception/exercise/Triangle.java
+++ A1022I1_module_2_tran_trieu/src/ss15_exception/exercise/Triangle.java
@@ -12,16 +12,16 @@ public class Triangle {
             int b = scanner.nextInt();
             int c = scanner.nextInt();
             if (a < 0 || b < 0 || c < 0) {
-                throw new IllegalTriangleException("Do not enter negative numbers");
+                throw new IllegalTriangleException("Do not enter negative numbers!");
             }
             if (a + b < c || a + c < b || b + c < a) {
-                throw new IllegalTriangleException("The sum of any two sides must be greater than the other");
+                throw new IllegalTriangleException("The sum of any two sides must be greater than the other!");
             }
             System.out.println("This is triangle!");
         } catch (InputMismatchException e) {
             System.out.println("Input value must be numbers!");
         } catch (IllegalTriangleException e) {
-            System.out.println(e.getMessenger());
+            System.out.println(e.getMessage());
         } catch (Exception e) {
             System.out.println("Something error!");
         } finally {
