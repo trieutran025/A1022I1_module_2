dc28144c19139fcfa6d5e94f9eb6eac2879e2548
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-03-06 17:58:25 +0700

    src

--- A1022I1_module_2_tran_trieu/src/ss15_exception/exercise/IllegalTriangleException.java
+++ A1022I1_module_2_tran_trieu/src/ss15_exception/exercise/IllegalTriangleException.java
@@ -1,23 +1,9 @@
 package ss15_exception.exercise;
 
 public class IllegalTriangleException extends Exception {
-    private int a;
-    private int b;
-    private int c;
 
-    public IllegalTriangleException(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public IllegalTriangleException(String message) {
+        super(message);
     }
 
-    public String getMessage() {
-        if (a < 0 || b < 0 || c < 0) {
-            return "Do not enter negative numbers";
-        } else if (a + b < c || a + c < b || b + c < a) {
-            return "The sum of any two sides must be greater than the other";
-        } else {
-            return "";
-        }
-    }
 }
--- A1022I1_module_2_tran_trieu/src/ss15_exception/exercise/Triangle.java
+++ A1022I1_module_2_tran_trieu/src/ss15_exception/exercise/Triangle.java
@@ -12,10 +12,10 @@ public class Triangle {
             int b = scanner.nextInt();
             int c = scanner.nextInt();
             if (a < 0 || b < 0 || c < 0) {
-                throw new IllegalTriangleException(a, b, c);
+                throw new IllegalTriangleException("Do not enter negative numbers");
             }
             if (a + b < c || a + c < b || b + c < a) {
-                throw new IllegalTriangleException(a, b, c);
+                throw new IllegalTriangleException("The sum of any two sides must be greater than the other");
             }
             System.out.println("This is triangle!");
         } catch (InputMismatchException e) {
