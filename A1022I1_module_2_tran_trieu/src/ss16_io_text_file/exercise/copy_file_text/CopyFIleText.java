3137b7e686b9234f4a771f6140949edd0f8f33d8
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-03-10 21:19:16 +0700

    src

--- A1022I1_module_2_tran_trieu/src/ss16_io_text_file/exercise/copy_file_text/CopyFIleText.java
+++ A1022I1_module_2_tran_trieu/src/ss16_io_text_file/exercise/copy_file_text/CopyFIleText.java
@@ -2,16 +2,17 @@ package ss16_io_text_file.exercise.copy_file_text;
 
 
 import java.io.*;
+import java.util.Scanner;
 
 public class CopyFIleText {
 
-    public static final String FILE_SOURCE = "D:\\CodeGym\\module_2\\A1022I1_module_2\\A1022I1_module_2_tran_trieu\\src\\ss16_io_text_file\\exercise\\source\\file.txt";
-    public static final String FILE_DEST = "D:\\CodeGym\\module_2\\A1022I1_module_2\\A1022I1_module_2_tran_trieu\\src\\ss16_io_text_file\\exercise\\destination\\file.txt";
-
     public static void main(String[] args) {
         try {
-            File sourceFile = new File(FILE_SOURCE);
-            File destFile = new File(FILE_DEST);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter source file:");
+            File sourceFile = new File(scanner.nextLine());
+            System.out.print("Enter dest file:");
+            File destFile = new File(scanner.nextLine());
             BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
             BufferedWriter writer = new BufferedWriter(new FileWriter(destFile));
 
